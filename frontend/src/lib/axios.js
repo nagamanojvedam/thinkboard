6f2bd2226e81fc561a852f81a058456be294394a
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2025-06-19 22:27:11 +0530

    fixed config.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { BASE_URL } from "./config";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api/v1"
+    : "/api/v1";
 
 export default axios.create({
   baseURL: BASE_URL,
